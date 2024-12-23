0fe1a89052ae6c51b890be058f51acae17304a3c
HyunmoPark <gusah0p@gmail.com>
2024-12-23 15:20:52 +0900

    2단계 완료

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        System.out.println("==명언 앱==");
-        System.out.println("명령)종료");
+public class Main {
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
 
     }
 }
+
+    class App{
+        public void run(){
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("==명언 앱==");
+
+            System.out.print("명언 : ");
+            scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다리며 엔터를 쳐야 입력 완료 후 넘어감.
+
+            System.out.print("작가 : ");
+            scanner.nextLine();
+            System.out.println("1번 명언이 등록되었습니다.");
+            System.out.println("명령)종료");
+        }
+    }
+
